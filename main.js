9c49e32f73268c3c8f9c439cc938bf44fde9a2cf
José Simões <jose.simoes@estudante.ufjf.br>
2023-11-21 22:29:19 -0300

    Adiciona conteudo ao main.js e listas.js

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+const { text } = require("node:stream/consumers");
+
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
-const li = document.createElement("li");
-li.textContent = "Primeiro";
-olItens.appendChild(li);
+
+
+adicionaElementoNaLista("Primeiro");
+adicionaElementoNaLista("Segundo");
+adicionaElementoNaLista("Terceiro");
+
+
+
+function adicionaElementoNaLista(texto){
+    const li = document.createElement("li");
+    li.textContent = texto;
+    olItens.appendChild(li);
+}
+
