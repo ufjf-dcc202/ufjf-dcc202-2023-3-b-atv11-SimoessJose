b1d80443e1edce880c4025d055bead48153feff8
José Simões <jose.simoes@estudante.ufjf.br>
2023-12-04 23:37:23 -0300

    adiciona ajustes nas funçoes

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
+import { adicionaNaLista, getLista, limpaLista, removeDaLista } from "./lista.js";
 
-import { getLista } from "./src/lista.js";
+// Passo 1 - Ache os elementos importantes
+const pEntrada = document.querySelector('#entrada');
+const olSaida = document.querySelector('#itens');
+const btnAdicionar = document.querySelector('#adicionar');
+const btnLimpar = document.querySelector('#limpar');
+const btnRemover = document.querySelector('#remover');
 
-const pEntrada = document.querySelector("#entrada");
-const btnAdicionar = document.querySelector("#adicionar");
-const btnLimpar = document.querySelector("#limpar");
-const olItens = document.querySelector("#itens");
+btnLimpar.addEventListener('click', limparElementosDaLista);
+btnAdicionar.addEventListener('click', criaElementoNaLista);
+btnRemover.addEventListener('click', removeItemDaLista);
 
-atualizarListaOrdenada();
+atualizarItensDeLista();
 
-function atualizarListaOrdenada(){
+function criaElementoNaLista(){
+    const texto = pEntrada.textContent;
+    adicionaNaLista(texto);
+    atualizarItensDeLista();
+    pEntrada.textContent="";
+    pEntrada.focus();
+}
+
+function limparElementosDaLista(){
+    limpaLista();
+    atualizarItensDeLista();
+}
+
+function atualizarItensDeLista() {
+    olSaida.innerHTML = "";
     const lista = getLista();
-    for(let i = 0; i < lista.length; i++){
-        adicionaElementoNaListaOrdenada(lista[i]);
+    for (let i = 0; i < lista.length; i++){
+        const item = lista[i];
+        const li = document.createElement('li');
+        li.textContent = item;
+        olSaida.appendChild(li);
     }
 }
 
+function removeItemDaLista() {
+    removeDaLista();
+    atualizarItensDeLista();
 
-function adicionaElementoNaListaOrdenada(texto){
-    const li = document.createElement("li");
-    li.textContent = texto;
-    olItens.appendChild(li);
 }
-
